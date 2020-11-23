1e7a175cb700f5d04a7e0da4fdf0832a3875cb90
AsmaBALAMANE <ea_balamane@esi.dz>
2020-11-23 20:10:55 +0100

    add predictListProfiles and PredictListOffers endpoint

--- app.py
+++ app.py
@@ -33,10 +33,10 @@ def predict():
     job.append(request_json['job']['j_title'])
     job.append(request_json['job']['description'])
     
-    # get job as vector
+    # get profile as vector
     profile=[]
     profile.append(request_json['profile']['p_skills'])
-    profile.append(request_json['profile']['p_skills'])
+    profile.append(request_json['profile']['p_title'])
     profile.append(request_json['profile']['experiences'])
     
  
@@ -49,6 +49,87 @@ def predict():
     rates= {'rate_titles': matching[0],'rate_description': matching[1],'rate_skills': matching[2],'final_rate': matching[3]}
     response = flask.jsonify(rates)
     return response, 200
+
+
+@app.route('/predictListOffers', methods=['POST'])
+def predictListOffers():
+     
+    request_json = request.get_json()
+    # coefficient 
+    skills_rate  = float(request_json['skills_rate'])   
+    title_rate   = float(request_json['title_rate'])   
+    description_rate = float(request_json['description_rate'])  
+        
+     # get Profile as vector
+    profile=[]
+    profile.append(request_json['profile']['p_skills'])
+    profile.append(request_json['profile']['p_title'])
+    profile.append(request_json['profile']['experiences'])
+    
+
+    #models
+    w2v_skills = load_models('models/w2v_skills.p')
+    w2v_jobtitles = load_models('models/w2v_jobtitles.p')
+    w2v_description = load_models('models/w2v_description.p')  
+   
+    jobs=[]
+    rates_raw=[]  
+   
+    jobs= request_json['job']
+    for job in jobs:   
+        # get job as vector
+        job_raw=[] 
+        job_raw.append(job['j_skills'])
+        job_raw.append(job['j_title'])
+        job_raw.append(job['description'])
+        
+        # calculate matching rate 
+        matching= ps.job_profile_matching(skills_rate, title_rate, description_rate, job_raw, profile,w2v_skills, w2v_jobtitles, w2v_description)
+        rates= {'rate_titles': matching[0],'rate_description': matching[1],'rate_skills': matching[2],'final_rate': matching[3]}
+        rates_raw.append(rates)
+        
+    response = flask.jsonify(rates_raw)
+    return response, 200
+
+@app.route('/predictListProfiles', methods=['POST'])
+def predictListProfiles():
+     
+    request_json = request.get_json()
+    # coefficient 
+    skills_rate  = float(request_json['skills_rate'])   
+    title_rate   = float(request_json['title_rate'])   
+    description_rate = float(request_json['description_rate'])  
+
+    # get job as vector
+    job=[]
+    job.append(request_json['job']['j_skills'])
+    job.append(request_json['job']['j_title'])
+    job.append(request_json['job']['description'])
+
+    #models
+    w2v_skills = load_models('models/w2v_skills.p')
+    w2v_jobtitles = load_models('models/w2v_jobtitles.p')
+    w2v_description = load_models('models/w2v_description.p')  
+   
+    profiles=[]
+    rates_raw=[]  
+   
+    profiles= request_json['profile']
+    for profile in profiles:   
+        # get job as vector
+        profile_raw=[] 
+        profile_raw.append(profile['p_skills'])
+        profile_raw.append(profile['p_title'])
+        profile_raw.append(profile['experiences'])
+        
+        # calculate matching rate 
+        matching= ps.job_profile_matching(skills_rate, title_rate, description_rate, job, profile_raw,w2v_skills, w2v_jobtitles, w2v_description)
+        rates= {'rate_titles': matching[0],'rate_description': matching[1],'rate_skills': matching[2],'final_rate': matching[3]}
+        rates_raw.append(rates)
+        
+    response = flask.jsonify(rates_raw)
+    return response, 200
+
 
 # deserialization
 def load_models(file_path):  
